96051d3cae16c0c000215038766a88ead248538c
1998Som <somesankarsantra199818@gmail.com>
2024-10-03 00:04:54 +0530

    commited

---
+++ js Variable/project/project2/project.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
+
+  if (height == "" || height < 0 || isNaN(height)) {
+    results.innerHTML = "please give a valid height";
+  } else if (weight == "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = "please give a valid weight";
+  } else {
+    const BMI = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `<span>${BMI}</span>`;
+    if (BMI < 18.6) {
+      results.innerText = `You are Under Waight ${BMI}`;
+    } else if (BMI >= 18.6 && BMI <= 24.9) {
+      results.innerText = `You are Normal ${BMI}`;
+    } else if (BMI > 24.9) {
+      results.innerText = `You are Overweight ${BMI}`;
+    }
+  }
+});
